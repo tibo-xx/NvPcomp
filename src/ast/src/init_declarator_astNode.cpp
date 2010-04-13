361511c16331a4d140e6159d0bb9d83e6933a304
Brett <brett@ubuntu.ubuntu-domain>
2010-04-12 19:57:33 -0700

    Fixed declaration error output

--- src/ast/src/init_declarator_astNode.cpp
+++ src/ast/src/init_declarator_astNode.cpp
@@ -118,31 +118,50 @@ bool init_declarator_astNode::addType(int token_type, NvPcomp::symNode *st_node,
 		   st_node->hasType(FLOAT_TK) ||
 		   st_node->hasType(INT_TK))
 	      {
-		error = "SYNTAX ERROR: '" + identifier + "' has two or more data types in delcaration.";
+		error = "SYNTAX ERROR: '" + identifier + "' has two or more data types in delcaration";
 		return false;
 	      }
 	      break;
 	      
 	    case SIGNED_TK:
-	    case UNSIGNED_TK:
-	      if ( st_node->hasType(SIGNED_TK) ||
-		   st_node->hasType(UNSIGNED_TK))
+	      if ( st_node->hasType(UNSIGNED_TK))
 	      {
-		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'signed' and 'unsigned'.";
+		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'signed' and 'unsigned'";
+		return false;
+	      }      
+	      break;
+	    case UNSIGNED_TK:
+	      if ( st_node->hasType(SIGNED_TK) )
+	      {
+		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'signed' and 'unsigned'";
 		return false;
 	      }      
 	      break;
 
 	    case LONG_TK:
-	    case SHORT_TK:
-	      if ( st_node->hasType(LONG_TK) ||
-		   st_node->hasType(SHORT_TK))
+	      if ( st_node->hasType(SHORT_TK))
 	      {
-		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'long' and 'short'.";
+		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'long' and 'short'";
+		return false;
+	      } 
+	      break;
+	    case SHORT_TK:
+	      if ( st_node->hasType(SHORT_TK))
+	      {
+		error = "SYNTAX ERROR: '" + identifier + "' specified as both 'long' and 'short'";
 		return false;
 	      }      
 	      break;
-
+	      
+	    /*
+	    case CONST_TK:
+	      if (nodeString != ":declarator EQUAL_TK initializer" && !st_node->hasType(TYPEDEF_NAME_TK))
+	      {
+		error = "SYNTAX ERROR: '" + identifier + "' specified as 'const' but not initilized";
+		return false;
+	      }    
+	    */ 
+	    
 	    // If we find 'typedef' in the types, we mark this as being a typedef by makeing its type a typedef_name
 	    case TYPEDEF_TK:
 		token_type = TYPEDEF_NAME_TK;
